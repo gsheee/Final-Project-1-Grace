4df6c4d16e1862ef23426add7bb2fdc90d6d7cc5
gsheee <graceshe0912@gmail.com>
2024-12-26 21:00:37 -0500

    Variables & Basic Arithmetic

---
+++ basics.swift
@@ -0,0 +1,7 @@
+//
+//  basics.swift
+//  
+//
+//  Created by Grace She on 2024-12-17.
+//
+
---
+++ testing.swift
@@ -0,0 +1,8 @@
+//
+//  testing.swift
+//  
+//
+//  Created by Grace She on 2024-12-16.
+//
+
+import Foundation
