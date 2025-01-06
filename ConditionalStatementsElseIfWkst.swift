32a383d2e984641ceed605dd62cddc6f990f9de9
gsheee <graceshe0912@gmail.com>
2025-01-06 18:31:49 -0500

    added solutions to third question using switches

--- ConditionalStatementsElseIfWkst.swift
+++ ConditionalStatementsElseIfWkst.swift
@@ -30,7 +30,25 @@ Total Price: $\(String(format: "%.2f", totalPrice))
 
 print("Enter your letter grade (A/B/C/D/E): ", terminator: "")
 let letterGrade = readLine()
+let grade = letterGrade!
 
+// switch method
+switch grade {
+case "A":
+    print("Your grade ranges from 80 - 100.")
+case "B":
+    print("Your grade ranges from 70 - 79.")
+case "C":
+    print("Your grade ranges from 60 - 69.")
+case "D":
+    print("Your grade ranges from 50 - 59.")
+case "E":
+    print("Your grade ranges from 0 - 49.")
+default: 
+    print("Invalid letter grade.")
+}
+
+// if statement method
 if (letterGrade! == "A") {
     print("Your grade ranges from 80 - 100.")
 } else if (letterGrade! == "B") {
